129ca8812513fe7b7b03f67d2ce32e76da7da3c1
MartkCz <martk494@gmail.com>
2025-10-19 00:24:33 +0200

    feat: add fromReflectionType() and fromBuiltInType() to TypeSchemaHelper

---
+++ src/Exception/UnsupportedTypeException.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+
+namespace Shredio\TypeSchema\Exception;
+
+final class UnsupportedTypeException extends RuntimeException
+{
+
+}
--- src/Helper/TypeSchemaHelper.php
+++ src/Helper/TypeSchemaHelper.php
@@ -2,15 +2,118 @@
 
 namespace Shredio\TypeSchema\Helper;
 
+use ReflectionIntersectionType;
+use ReflectionNamedType;
+use ReflectionType;
+use ReflectionUnionType;
 use Shredio\TypeSchema\Context\TypeContext;
 use Shredio\TypeSchema\Error\ErrorCollection;
 use Shredio\TypeSchema\Error\ErrorElement;
 use Shredio\TypeSchema\Error\ErrorPath;
+use Shredio\TypeSchema\Exception\UnsupportedTypeException;
 use Shredio\TypeSchema\Types\Type;
 use Shredio\TypeSchema\TypeSchema;
 
 final readonly class TypeSchemaHelper
 {
+
+	private const array SupportedBuiltinTypes = [
+		'bool' => true,
+		'float' => true,
+		'int' => true,
+		'mixed' => true,
+		'null' => true,
+		'object' => true,
+		'string' => true,
+	];
+
+	/**
+	 * @return Type<mixed>
+	 *
+	 * @throws UnsupportedTypeException
+	 */
+	public static function fromReflectionType(ReflectionType $type): Type
+	{
+		if ($type instanceof ReflectionNamedType) {
+			if ($type->isBuiltin()) {
+				$typeInstance = match ($type->getName()) {
+					'int' => TypeSchema::get()->int(),
+					'float' => TypeSchema::get()->float(),
+					'string' => TypeSchema::get()->string(),
+					'bool' => TypeSchema::get()->bool(),
+					'object' => TypeSchema::get()->object(),
+					'mixed' => TypeSchema::get()->mixed(),
+					'null' => TypeSchema::get()->null(),
+					default => throw new UnsupportedTypeException(sprintf('Unsupported builtin type: %s', $type->getName())),
+				};
+			} else {
+				$typeInstance = TypeSchema::get()->mapper($type->getName()); // @phpstan-ignore-line
+			}
+
+			if ($type->allowsNull() && $type->getName() !== 'null') {
+				$typeInstance = TypeSchema::get()->nullable($typeInstance);
+			}
+
+			return $typeInstance;
+		}
+
+		if ($type instanceof ReflectionUnionType) {
+			$types = [];
+			$isNullable = false;
+			foreach ($type->getTypes() as $unionType) {
+				if ($unionType instanceof ReflectionNamedType && $unionType->getName() === 'null') {
+					$isNullable = true;
+					continue;
+				}
+
+				$types[] = self::fromReflectionType($unionType);
+			}
+
+			$typeInstance = TypeSchema::get()->union($types); // @phpstan-ignore-line
+			if ($isNullable) {
+				$typeInstance = TypeSchema::get()->nullable($typeInstance);
+			}
+
+			return $typeInstance;
+		}
+
+		if ($type instanceof ReflectionIntersectionType) {
+			throw new UnsupportedTypeException('Intersection types are not supported yet.');
+		}
+
+		throw new UnsupportedTypeException(sprintf('Unknown ReflectionType: %s', $type::class));
+	}
+
+	/**
+	 * @return Type<mixed>
+	 *
+	 * @throws UnsupportedTypeException
+	 */
+	public static function fromBuiltInType(string $type, bool $nullable = false): Type
+	{
+		if (!isset(self::SupportedBuiltinTypes[$type])) {
+			throw new UnsupportedTypeException(sprintf('Unsupported builtin type: %s', $type));
+		}
+
+		if ($type === 'null') {
+			return TypeSchema::get()->null();
+		}
+
+		$typeObject = match ($type) {
+			'int' => TypeSchema::get()->int(),
+			'float' => TypeSchema::get()->float(),
+			'string' => TypeSchema::get()->string(),
+			'bool' => TypeSchema::get()->bool(),
+			'object' => TypeSchema::get()->object(),
+			'mixed' => TypeSchema::get()->mixed(),
+		};
+
+		if ($nullable) {
+			$typeObject = TypeSchema::get()->nullable($typeObject);
+		}
+
+		return $typeObject;
+	}
 
 	/**
 	 * @template TKey of array-key
