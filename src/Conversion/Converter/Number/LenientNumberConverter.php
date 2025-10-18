6f23ee5dc6585bd3f05d50e8cf3b1e80784256f9
MartkCz <martk494@gmail.com>
2025-10-19 00:22:12 +0200

    feat: LenientNumberConverter is stricter

--- src/Conversion/Converter/Number/LenientNumberConverter.php
+++ src/Conversion/Converter/Number/LenientNumberConverter.php
@@ -5,10 +5,29 @@ namespace Shredio\TypeSchema\Conversion\Converter\Number;
 final readonly class LenientNumberConverter implements NumberConverter
 {
 
+	public function __construct(
+		private bool $checkFloatPrecisionOnCastToInt = true,
+	)
+	{
+	}
+
 	public function int(mixed $value): ?int
 	{
 		if (is_int($value)) {
 			return $value;
+		}
+
+		if (is_float($value)) {
+			if (!$this->checkFloatPrecisionOnCastToInt) {
+				return (int) $value;
+			}
+
+			$int = (int) $value;
+			if ((float) $int === $value) {
+				return $int;
+			}
+
+			return null;
 		}
 
 		if (is_string($value)) {
@@ -16,10 +35,6 @@ final readonly class LenientNumberConverter implements NumberConverter
 			if ($filtered !== false) {
 				return $filtered;
 			}
-		}
-
-		if (is_float($value)) {
-			return (int) $value;
 		}
 
 		return null;
@@ -35,10 +50,13 @@ final readonly class LenientNumberConverter implements NumberConverter
 			return (float) $value;
 		}
 
-		if (is_string($value)) {
-			$filtered = filter_var($value, FILTER_VALIDATE_FLOAT);
-			if ($filtered !== false) {
-				return $filtered;
+		if (is_string($value) && $value !== '') {
+			if (ctype_digit($value)) {
+				return (float) $value;
+			}
+
+			if (preg_match("/^[+-]?(?:\d+(?:[.]\d*)?(?:[eE][+-]?\d+)?|[.]\d+(?:[eE][+-]?\d+)?)$/", $value)) {
+				return (float) $value;
 			}
 		}
 
